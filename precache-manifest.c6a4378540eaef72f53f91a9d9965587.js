e73c322b3c45465666f92fd36e11c4506c5563c8
gustavo andrade <gustavo64522@gmail.com>
2020-05-16 21:29:30 -0300

    Updates

---
+++ precache-manifest.c6a4378540eaef72f53f91a9d9965587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9313b4b645dbcaec6627e7883af2a8f9",
+    "url": "/Gustavo64522/Calculadora/index.html"
+  },
+  {
+    "revision": "81a3b0ed3f9381412298",
+    "url": "/Gustavo64522/Calculadora/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6668b09ed9b4452aac27",
+    "url": "/Gustavo64522/Calculadora/static/css/main.cba846c9.chunk.css"
+  },
+  {
+    "revision": "81a3b0ed3f9381412298",
+    "url": "/Gustavo64522/Calculadora/static/js/2.12765b7d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Gustavo64522/Calculadora/static/js/2.12765b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6668b09ed9b4452aac27",
+    "url": "/Gustavo64522/Calculadora/static/js/main.188dc139.chunk.js"
+  },
+  {
+    "revision": "83ad8819462194e6ee65",
+    "url": "/Gustavo64522/Calculadora/static/js/runtime-main.31989400.js"
+  }
+]);
